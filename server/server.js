a92ba9807936d2ab7a0c9c3f02f28c1bb1f00540
Jeff Smith <jeff.smith@endurance.com>
2015-12-17 17:26:58 -0700

    Adding passport authentication

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
-var app  = require('express')();
+var express = require('express');
+var app  = express();
 var http = require('http').Server(app);
 var io   = require('socket.io')(http);
+var passport = require('passport');
+var flash    = require('connect-flash');
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
